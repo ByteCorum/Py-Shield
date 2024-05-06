f22482122574b1143ce774c80a203da69be43ec6
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2024-05-06 12:08:45 +0300

    build

---
+++ build.cmd
@@ -0,0 +1,2 @@
+python.exe -m nuitka --quiet --follow-imports --remove-output --onefile --output-filename=py-sheild --windows-icon-from-ico=icon.ico --company-name="ByteCorum" --product-name="Py-Sheild" --file-version=1.0.0.0 --product-version=1.0.0.0 --file-description="Program/Library for Python created to protect your code from decompilation and detection by antiviruses" --copyright="https://github.com/ByteCorum/Py-Sheild/blob/main/LICENSE" main.py
+pause null

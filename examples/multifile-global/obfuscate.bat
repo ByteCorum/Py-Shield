b8185ab50bef64840b9f949192f5b7d9c92e3f20
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2024-08-05 14:23:57 +0300

    added examples

---
+++ examples/multifile-global/obfuscate.bat
@@ -0,0 +1,2 @@
+py-sheild --mode hashstr;crypt;looping;aes --loops 3 --output obfuscated --follow imports --files lib.py main.py
+pause null
---
+++ examples/multifile-legacy/obfuscate.bat
@@ -0,0 +1,2 @@
+py-sheild-legacy --loops 3 --mode 4 --output obfuscated --file main.py;lib.py
+pause null

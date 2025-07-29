5ad90e360a95b7e0863dda694379cb1d67ae3a24
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-07-29 22:41:27 +0300

    changed proj description

--- scr/build-mac.sh
+++ scr/build-mac.sh
@@ -10,6 +10,6 @@ python -m nuitka \
     --product-name="Py-Shield" \
     --file-version=3.0.0.0 \
     --product-version=3.0.0.0 \
-    --file-description="Program/Library for Python created to protect your code from decompilation and detection by antiviruses" \
+    --file-description="Tool/Library for Python used to obfuscate and protect your code in static and runtime from decompilation, reverse debug, etc. Also, can prevent detection by antiviruses." \
     --copyright="https://github.com/ByteCorum/Py-Shield/blob/main/LICENSE" \
     main.py
--- scr/config.py
+++ scr/config.py
@@ -4,7 +4,7 @@ NAME = "Py-Shield"
 AUTHOR = "ByteCorum"
 URL = "https://github.com/ByteCorum/Py-Shield"
 VERSION = "3.0.0.0"
-DESCRIPTION = "Tool/Library for Python used to obfuscate and protect your code from decompilation, reverse engineering, etc. Also, can prevent detections by antiviruses."
+DESCRIPTION = "Tool/Library for Python used to obfuscate and protect your code in static and runtime from decompilation, reverse debug, etc. Also, can prevent detection by antiviruses."
 
 class Command(ABC):
     exclusiveOptions: list

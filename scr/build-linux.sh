3b4a903f26fa4e1b60ad731abe633cee88a86936
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-07-29 13:51:21 +0300

    added build

---
+++ scr/build-linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python -m nuitka \
+    --quiet \
+    --follow-imports \
+    --remove-output \
+    --onefile \
+    --output-filename=py-shield \
+    --linux-icon=../assets/icon.png \
+    --company-name="ByteCorum" \
+    --product-name="Py-Shield" \
+    --file-version=3.0.0.0 \
+    --product-version=3.0.0.0 \
+    --file-description="Program/Library for Python created to protect your code from decompilation and detection by antiviruses" \
+    --copyright="https://github.com/ByteCorum/Py-Shield/blob/main/LICENSE" \
+    main.py
+
+echo "Build completed. Press any key to continue..."
+read -n 1
---
+++ scr/build-win.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+python -m nuitka ^
+    --quiet ^
+    --follow-imports ^
+    --remove-output ^
+    --onefile ^
+    --output-filename=py-shield ^
+    --windows-icon-from-ico=../assets/icon.ico ^
+    --company-name="ByteCorum" ^
+    --product-name="Py-Shield" ^
+    --file-version=3.0.0.0 ^
+    --product-version=3.0.0.0 ^
+    --file-description="Program/Library for Python created to protect your code from decompilation and detection by antiviruses" ^
+    --copyright="https://github.com/ByteCorum/Py-Shield/blob/main/LICENSE" ^
+    main.py
+
+echo.
+echo Build completed successfully!
+pause
